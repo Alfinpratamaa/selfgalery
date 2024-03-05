91fbacf63c8d8c3d0f20502fa04e6adbf41553cf
muhamadalfinpratama <muhamadalfinpratamaa@gmail.com>
2024-03-05 18:31:05 +0700

    fix bug

--- src/app/still-loggedin/page.tsx
+++ src/app/still-loggedin/page.tsx
@@ -1,26 +1,28 @@
 import { Button } from '@/components/ui/button';
 import { signOut } from 'next-auth/react';
 import { redirect } from 'next/navigation';
-import React from 'react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 
-const LogoutPage: React.FC = () => {
+const UserStillLoggedIn = () => {
 
     const session = getServerSession(authOptions)
 
     if (!session) {
         redirect('/login')
     }
+
+    const handleLogout = async () => {
+        await signOut()
+        redirect('/login')
+    }
+
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
             <h1 className="text-3xl font-bold mb-4">You haven't logged out before.</h1>
             <p className="text-lg text-gray-600 mb-8">Please logout first.</p>
             <div className='flex flex-col justify-center mx-auto'>
-                <Button size={'sm'} color='primary' onClick={() => signOut({
-                    redirect: true,
-                    callbackUrl: '/login',
-                })}>
+                <Button size={'sm'} color='primary' onClick={handleLogout}>
                     Logout
                 </Button>
                 <p>or back to home</p>
@@ -32,4 +34,4 @@ const LogoutPage: React.FC = () => {
     );
 };
 
-export default LogoutPage;
+export default UserStillLoggedIn;
