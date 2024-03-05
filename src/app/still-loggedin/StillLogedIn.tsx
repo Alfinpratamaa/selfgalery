700c8bf849caafde846f8d4ee6a64b226f56cc34
muhamadalfinpratama <muhamadalfinpratamaa@gmail.com>
2024-03-05 19:17:38 +0700

    none

--- src/app/still-loggedin/StillLogedIn.tsx
+++ src/app/still-loggedin/StillLogedIn.tsx
@@ -15,21 +15,20 @@ const UserStillLoggedIn = () => {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <div className="flex flex-col mt-[-100px] items-center justify-center  bg-gray-100">
             <h1 className="text-3xl font-bold mb-4">You haven't logged out before.</h1>
-            <p className="text-lg text-gray-600 mb-8">Please logout first.</p>
+            <p className="text-lg text-gray-600 mb-2">Please logout first.</p>
             <div className='flex flex-col justify-center mx-auto'>
                 <Button size={'default'} color='primary' onClick={handleLogout}>
                     Logout
                 </Button>
-                <p className='mt-2 mb-2'>or back to home</p>
-                <Link href='/'>
-                    <div className='mx-auto justify-center'>
-                        <Button size={'default'} variant='ghost'  >
+                <p className='mt-2 mb-2 text-gray-600'>or back to home</p>
+                <Button size={'default'} variant='ghost'  >
+                    <Link href='/'>
                         Home
-                    </Button>
-                    </div>
-                </Link>
+                    </Link>
+                </Button>
+
             </div>
         </div>
     );
